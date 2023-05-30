525b4878605db5b9a7677a0df7b30d03f77f7a59
phucthien432002 <103552445+phucthien432002@users.noreply.github.com>
2023-05-30 17:56:22 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -83,7 +83,139 @@
         </div>
       </div>
     </header>
-    <slider></slider>
+    <div class="main1">
+      <div id="slider">
+        <div class="list">
+          <div class="item active">
+            <div class="slogan-placeholder">
+              <h1>Welcome to iFi audio</h1>
+              <span
+                >We passionately believe that everyone deserves the best when it comes to
+                sound. Our products are made to high technological standards to give you
+                an authentic, passionate and empowering music experience. Hear it. Feel
+                it.</span
+              >
+              <a class="take-tour-button">Take a tour</a>
+            </div>
+            <img src="../static/images/Header/ifi-sig (2).png" alt="" />
+          </div>
+          <a class="item" href="https://3kshop.vn/ifi-lan-isilencer/" target="_blank">
+            <div class="product-info new">
+              <h1>LAN iSilencer</h1>
+              <h2>Your network. Silenced.</h2>
+              <span>No more distorted audio signal</span>
+            </div>
+            <img src="../static/images/Header/DSCF5033.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-go-bar/" target="_blank">
+            <div class="product-info">
+              <h1>GO bar</h1>
+              <h2>Best bar none.</h2>
+              <span
+                >Premium USB DAC. World's most powerful headphone amp of its size</span
+              >
+            </div>
+            <img src="../static/images/Header/go-bar-hero.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-go-link/" target="_blank">
+            <div class="product-info new">
+              <h1>GO link</h1>
+              <h2>The headphone dongle linking you with your music</h2>
+              <span>Superior sound with a twist</span>
+            </div>
+            <img src="../static/images/Header/GO-link-1.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-go-pod/" target="_blank">
+            <div class="product-info new">
+              <h1>GO pod</h1>
+              <h2>World-class. Wireless</h2>
+              <span>Wearable HD Bluetooth DAC and headphone amp</span>
+            </div>
+            <img src="../static/images/Header/go-pod-hero.jpg" alt="" />
+          </a>
+          <a
+            class="item"
+            href="https://3kshop.vn/ifi-gioi-thieu-zen-dac-signature-va-zen-can-signature-phien-ban-dac-biet/"
+            target="_blank"
+          >
+            <div class="product-info sale">
+              <h1>ZEN CAN Signature Set MZ99 & Meze 99 Classics</h1>
+              <h2>Limited Time Offer - Save up to 25%</h2>
+            </div>
+            <img src="../static/images/Header/mz99-meze-set.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-neo-stream/" target="_blank">
+            <div class="product-info">
+              <h1>NEO Stream</h1>
+              <h2>Network audio streamer</h2>
+              <span>Built by passionate enthusiasts, for passionate enthusiasts</span>
+            </div>
+            <img src="../static/images/Header/neos1.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-uno/" target="_blank">
+            <div class="product-info new">
+              <h1>uno</h1>
+              <h2>The new sonic supercharger from iFi</h2>
+              <span>Bigger. Bolder. Better Sound.</span>
+            </div>
+            <img src="../static/images/Header/uno-hero.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-xdsd-gryphon/" target="_blank">
+            <div class="product-info">
+              <h1>xDSD Gryphon</h1>
+              <h2>A hi-fi system in your pocket</h2>
+              <span>Premium HD DAC + Powerful Balanced headamp</span>
+            </div>
+            <img src="../static/images/Header/xdsd-g-hero.jpg" alt="" />
+          </a>
+          <a class="item" href="https://3kshop.vn/ifi-zen-air-blue/" target="_blank">
+            <div class="product-info">
+              <h1>ZEN Air Blue</h1>
+              <h2>Let the Air in.</h2>
+              <span
+                >Enjoy unbeatable HD Bluetooth streaming from your phone, tablet or
+                computer.</span
+              >
+            </div>
+            <img src="../static/images/Header/zab.jpg" alt="" />
+          </a>
+          <a
+            class="item"
+            href="https://3kshop.vn/ifi-ican-phantom-headphone-amplifier-doc-dao-gia-3-700/"
+            target="_blank"
+          >
+            <div class="product-info new">
+              <h1>iCAN Phantom</h1>
+              <h2>Vanish into the music</h2>
+              <span
+                >NEW reference-class analogue headphone amp with incorporated
+                electrostatic energiser with advanced user interface and network-connected
+                control system</span
+              >
+            </div>
+            <img src="../static/images/Header/iFi-Phantom_0136.jpg" alt="" />
+          </a>
+        </div>
+        <div class="button-placeholder">
+          <button id="prev"><</button>
+          <button id="next">></button>
+        </div>
+        <ul class="dots">
+          <li class="active"></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div id="product"></div>
+    </div>
     <v-main>
       <!-- <v-app-bar
 			elevation="60"
@@ -148,21 +280,138 @@
 		</v-app-bar> -->
       <Nuxt />
     </v-main>
-    <v-footer> </v-footer>
+    <v-footer></v-footer>
   </v-app>
 </template>
 
 <script>
-import slider from "../components/slider.vue";
+// import slider from "../components/slider.vue";
 export default {
-  components: { slider },
+  // components: { slider },
   name: "DefaultLayout",
+  mounted() {
+    const list = this.$el.querySelector("#slider .list");
+    const items = this.$el.querySelectorAll("#slider .list .item");
+    const dots = this.$el.querySelectorAll("#slider .dots li");
+    const prev = this.$el.querySelector("#prev");
+    const next = this.$el.querySelector("#next");
+    let active = 0;
+    const countItem = items.length;
+    const autoRef = setInterval(() => {
+      next.click();
+    }, 5000);
+
+    next.onclick = function nextItem() {
+      if (active + 1 >= countItem) {
+        active = 0;
+      } else {
+        active = active + 1;
+      }
+      reloadSlide();
+    };
+
+    prev.onclick = function prevItem() {
+      if (active - 1 < 0) {
+        active = countItem - 1;
+      } else {
+        active = active - 1;
+      }
+      reloadSlide();
+    };
+
+    dots.forEach((li, key) => {
+      li.addEventListener("click", function () {
+        active = key;
+        reloadSlide();
+      });
+    });
+
+    function reloadSlide() {
+      items.forEach((item, index) => {
+        if (index === active) {
+          item.classList.add("active");
+        } else {
+          item.classList.remove("active");
+        }
+      });
+
+      const activeDot = this.$el.querySelector("#slider .dots li.active");
+      activeDot.classList.remove("active");
+      dots[active].classList.add("active");
+    }
+
+    const proList = this.$el.querySelector(
+      "#header .header-bar .navigation-placeholder .product-list"
+    );
+    const toolList = this.$el.querySelector(
+      "#header .header-bar .navigation-placeholder .tool-box"
+    );
+    const supList = this.$el.querySelector(
+      "#header .header-bar .navigation-placeholder .support-box"
+    );
+
+    const proMenu = this.$el.querySelector(
+      "#header .header-bar .navigation-placeholder .product-list .dropdown-menu"
+    );
+    const toolMenu = this.$el.querySelector(
+      "#header .header-bar .navigation-placeholder .tool-box .dropdown-menu"
+    );
+    const supMenu = this.$el.querySelector(
+      "#header .header-bar .navigation-placeholder .support-box .dropdown-menu"
+    );
+
+    const dropCheck = this.$el.querySelector(
+      "#header .header-bar .logo-placeholder .dropbtn"
+    );
+    const nav = this.$el.querySelector("#header .header-bar .navigation-placeholder");
+
+    proList.onclick = function () {
+      if (proMenu.classList.contains("active")) {
+        proMenu.classList.remove("active");
+      } else {
+        proMenu.classList.add("active");
+      }
+    };
+
+    toolList.onclick = function () {
+      if (toolMenu.classList.contains("active")) {
+        toolMenu.classList.remove("active");
+      } else {
+        toolMenu.classList.add("active");
+      }
+    };
+
+    supList.onclick = function () {
+      if (supMenu.classList.contains("active")) {
+        supMenu.classList.remove("active");
+      } else {
+        supMenu.classList.add("active");
+      }
+    };
+
+    dropCheck.onclick = function () {
+      if (nav.classList.contains("show")) {
+        nav.classList.remove("show");
+        proMenu.classList.remove("active");
+        toolMenu.classList.remove("active");
+        supMenu.classList.remove("active");
+      } else {
+        nav.classList.add("show");
+      }
+    };
+  },
 };
 </script>
 <style>
+body {
+  overflow-x: hidden;
+}
+
 #header {
   position: fixed;
-  width: 100%;
+  top: 0;
+  left: 0;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -171,14 +420,15 @@ export default {
 }
 
 .header-bar {
-  width: 100vw;
+  width: 100%;
+  padding: 10px 50px;
   display: flex;
   background-color: rgba(255, 255, 255);
 }
 
 .logo-placeholder {
-  margin-left: 10px;
-  width: 100%;
+  width: 10%;
+  display: flex;
   justify-content: center;
   align-items: center;
 }
@@ -188,13 +438,12 @@ export default {
 }
 
 .navigation-placeholder {
-  margin-right: 24px;
   width: 90%;
   display: flex;
   align-items: center;
   color: #ff5c23;
   justify-content: right;
-  gap: 50px;
+  gap: 100px;
 }
 
 .navigation-placeholder .product-list,
@@ -203,15 +452,16 @@ export default {
   position: relative;
   cursor: pointer;
 }
+
 /* .navigation-placeholder .product-list{
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    width: 70%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 70%;
 }
 .navigation-placeholder .product-list a{
-    color: #ff5c23;
-    text-decoration: none;
+  color: #ff5c23;
+  text-decoration: none;
 } */
 
 .navigation-placeholder .dropdown-menu {
@@ -267,68 +517,186 @@ export default {
   text-decoration: none;
   color: #ff5c23;
 }
-/* .navigation-placeholder .product-list{
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    width: 70%;
-}
-.navigation-placeholder .product-list a{
-    color: #ff5c23;
-    text-decoration: none;
-} */
 
-.navigation-placeholder .dropdown-menu {
+main {
   display: flex;
   flex-direction: column;
-  visibility: hidden;
-  position: absolute;
-  min-width: 240px;
-  left: -94px;
-  padding: 10px;
-  font-size: 20px;
-  gap: 10px;
-  background: white;
-  border-radius: 0 0 15px 15px;
-  opacity: 0;
-  transition: 0.5s;
-  padding-top: 20px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
-    rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  height: 100vh;
+  width: 100vw;
 }
 
-.navigation-placeholder .dropdown-menu a {
-  text-decoration: none;
-  color: black;
-  border-radius: 10px;
-  padding-left: 10px;
-  gap: 5px;
+#slider {
+  overflow-x: hidden;
+}
+
+.list {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
   display: flex;
+  transition: opacity 1.5s;
+}
+
+.list .item {
+  /* display: none; */
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 1.5s;
+}
+
+.list .item.active {
+  display: block;
+  opacity: 1;
+  z-index: 5;
+}
+
+.list img {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100vw;
+  object-fit: cover;
+}
+
+.list .item .slogan-placeholder {
+  position: absolute;
+  width: 50vw;
+  top: 30%;
+  left: 25vw;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
   align-items: center;
 }
 
-.navigation-placeholder .dropdown-menu a:hover {
+.list .item .slogan-placeholder h1 {
+  color: #ff5c23;
+  font-size: 30px;
+  margin-bottom: 15px;
+}
+
+.list .item .slogan-placeholder span {
+  font-size: 18px;
+  margin-bottom: 30px;
+}
+
+.list .item .take-tour-button {
+  padding: 10px 50px;
+  border-radius: 50px;
+  border: 3px solid #ff5c23;
+  cursor: pointer;
+  background-color: white;
+  color: #ff5c23;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.list .item .take-tour-button a {
+  text-decoration: none;
+}
+
+.list .item .product-info {
+  position: absolute;
+  min-width: 44vw;
+  bottom: 20%;
+  left: 8vw;
+  display: flex;
+  color: white;
+  flex-direction: column;
+  border-left: 8px solid #ff5c23;
+  padding-left: 16px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.6);
+}
+
+.list .item .product-info h1 {
+  font-size: calc(30px + 2vw);
+}
+
+.list .item .product-info h2 {
+  font-size: 30px;
+}
+
+.list .item .product-info span {
+  font-size: 24px;
+}
+
+.list .item .sale::before {
+  content: "Save";
+  border-radius: 20px;
+  width: 50px;
+  padding: 5px 10px;
+  font-size: 20px;
+  font-weight: 700;
   background-color: #ff5c23;
+  color: white;
+  text-align: center;
+}
+
+.list .item .new::before {
+  content: "New";
+  border-radius: 20px;
+  width: 50px;
+  padding: 5px 10px;
+  font-size: 20px;
+  font-weight: 700;
+  background-color: #ff5c23;
+  color: white;
+  text-align: center;
+}
+
+.button-placeholder {
+  position: absolute;
+  top: 45%;
+  left: 2%;
+  width: 96%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.button-placeholder button {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: none;
+  font-family: monospace;
+  font-weight: bold;
+  font-size: 30px;
+  transition: 0.3s;
+  z-index: 6;
+}
+
+.button-placeholder button:hover {
+  background-color: rgba(143, 143, 143, 0.568);
   color: white;
 }
 
-.navigation-placeholder .product-list .dropdown-menu {
-  left: -25px;
+.dots {
+  position: absolute;
+  bottom: 5px;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 7;
 }
 
-.navigation-placeholder .support-box .dropdown-menu {
-  left: -150px;
+.dots li {
+  list-style: none;
+  width: 10px;
+  height: 10px;
+  background-color: rgba(143, 143, 143, 0.568);
+  border-radius: 20px;
+  margin: 10px;
+  transition: 1s;
 }
 
-.navigation-placeholder .product-list:hover .dropdown-menu,
-.navigation-placeholder .tool-box:hover .dropdown-menu,
-.navigation-placeholder .support-box:hover .dropdown-menu {
-  visibility: visible;
-  opacity: 1;
-}
-
-.navigation-placeholder .account a {
-  text-decoration: none;
-  color: #ff5c23;
+.dots li.active {
+  width: 30px;
+  background-color: #ff5c23;
 }
 </style>
